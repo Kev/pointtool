512ec2152fca29576e479c625d405fd1258aa73e
Kevin Smith <git@kismith.co.uk>
2014-07-10 19:27:47 +0100

    Allow rubocop to autocorrect the code

--- main.rb
+++ main.rb
@@ -78,7 +78,7 @@ class Character
   include DataMapper::Resource
   property :id, Serial
   property :name, String
-  property :active, Boolean, :default => true
+  property :active, Boolean, default: true
   belongs_to :player
   has n, :attendances
   has n, :events, through: :attendances
@@ -116,22 +116,22 @@ class Event
   has 1, :approval
   belongs_to :submission
 
-  def getCharactersString()
-    result = ""
+  def getCharactersString
+    result = ''
     characters.each{|x|
-      result += ", " if not result.empty?
+      result += ', ' unless result.empty?
       result += x.name}
-    return result
+    result
   end
 
-  def getFormatDateTime()
-    return event_time.strftime("%Y-%m-%d %H:%M")
+  def getFormatDateTime
+    event_time.strftime('%Y-%m-%d %H:%M')
   end
 
   def allow_edit(player, is_admin)
     return true if is_admin
-    return false if self.approval
-    return self.submission.player == player
+    return false if approval
+    submission.player == player
   end
 end
 
@@ -147,13 +147,13 @@ Approval.auto_upgrade!
 
 def getPlayersActiveIn(events)
   players = []
-  events.each{|event| event.characters.each{|character| players << character.player if not players.include?(character.player)}}
-  players.sort!{|left,right| left.name <=> right.name}
-  return players
+  events.each { |event| event.characters.each { |character| players << character.player unless players.include?(character.player) } }
+  players.sort! { |left, right| left.name <=> right.name }
+  players
 end
 
-def getPendingEvents()
-  Event.all(:order => [:event_time], :approval => nil)
+def getPendingEvents
+  Event.all(order: [:event_time], approval: nil)
 end
 
 def getEvents(month, year)
@@ -162,13 +162,13 @@ def getEvents(month, year)
   Event.all(:order => [:event_time], :event_time.gte => monthStart, :event_time.lt => monthEnd)
 end
 
-def nowString()
+def nowString
   DateTime.now.to_s
 end
 
 def getFilteredEvents(month, year, filter)
   events = getEvents(month, year)
-  return filter ? events.map{|event| filter.call(event)}.compact : events
+  filter ? events.map { |event| filter.call(event) }.compact : events
 end
 
 def getNextLink(monthNumber, year, prefix)
@@ -179,9 +179,9 @@ def getNextLink(monthNumber, year, prefix)
     nextMonth = 1
     nextYear += 1
   end
-  url = $base_url + prefix + nextYear.to_s + "/" + nextMonth.to_s + "/"
-  month = Date::MONTHNAMES[nextMonth] + " " + nextYear.to_s
-  return Hash[:url => url, :month => month] if nowDate.month > monthNumber or nowDate.year > year
+  url = $base_url + prefix + nextYear.to_s + '/' + nextMonth.to_s + '/'
+  month = Date::MONTHNAMES[nextMonth] + ' ' + nextYear.to_s
+  return Hash[url: url, month: month] if nowDate.month > monthNumber or nowDate.year > year
 end
 
 def getPreviousLink(monthNumber, year, prefix)
@@ -191,63 +191,63 @@ def getPreviousLink(monthNumber, year, prefix)
     previousMonth = 12
     previousYear -= 1
   end
-  url = $base_url + prefix + previousYear.to_s + "/" + previousMonth.to_s + "/"
-  month = Date::MONTHNAMES[previousMonth] + " " + previousYear.to_s
-  return Hash[:url => url, :month => month]
+  url = $base_url + prefix + previousYear.to_s + '/' + previousMonth.to_s + '/'
+  month = Date::MONTHNAMES[previousMonth] + ' ' + previousYear.to_s
+  Hash[url: url, month: month]
 end
 
 def getMonthReport(monthNumber, year)
   @month = Date::MONTHNAMES[monthNumber]
   @pendingEventCount = 0
-  @events = getFilteredEvents(monthNumber, year, lambda{|event|
-    @pendingEventCount += 1 if not event.approval
-    return event if event.approval
-    })
+  @events = getFilteredEvents(monthNumber, year, lambda do|event|
+      @pendingEventCount += 1 unless event.approval
+      return event if event.approval
+    end)
   @players = getPlayersActiveIn(@events)
   @isk_total = 0
-  @events.each{|event| @isk_total += event.corner_value}
+  @events.each { |event| @isk_total += event.corner_value }
   @isk_total *= 0.8 # to allow for stuff selling below corner value
   @isk_total = @isk_total.floor
   @point_total = 0
   @max_points = 0
-  @players.each{|player|
+  @players.each do|player|
     player_points = player.points_from(@events)
     @point_total += player_points
     @max_points = [@max_points, player_points].max
-  }
+  end
   @isk_point_average = (@point_total > 0 ? @isk_total / @point_total : 0).floor
-  @show_approver = checkIsAdmin()
-  @previous_link = getPreviousLink(monthNumber, year, "/month/")
-  @next_link = getNextLink(monthNumber, year, "/month/")
+  @show_approver = checkIsAdmin
+  @previous_link = getPreviousLink(monthNumber, year, '/month/')
+  @next_link = getNextLink(monthNumber, year, '/month/')
   haml :month
 end
 
-def getCurrentPlayer()
+def getCurrentPlayer
   if session['player_id']
-    player = Player.first(:id => session['player_id'])
+    player = Player.first(id: session['player_id'])
     if player
       return player
     end
   end
 
   character_name = env['HTTP_EVE_CHARNAME']
-  if not character_name or character_name == ""
+  if not character_name or character_name == ''
     if settings.fake_igb_character
       character_name = settings.fake_igb_character
     end
   end
-  character = Character.first(:name => character_name, :active => true)
+  character = Character.first(name: character_name, active: true)
   if character
     return character.player
   end
 
   if settings.dev_mode
-    return Player.first_or_create(:name => "Default", :admin => true)
+    return Player.first_or_create(name: 'Default', admin: true)
   end
-  return nil
+  nil
 end
 
-def checkIsAdmin()
+def checkIsAdmin
   if settings.dev_mode
     if @logged_in_player
       return @logged_in_player.admin
@@ -256,52 +256,52 @@ def checkIsAdmin()
   end
   # Do not allow admin by players who haven't authenticated, even if they're admins
   if session['player_id']
-    player = Player.first(:id => session['player_id'])
+    player = Player.first(id: session['player_id'])
     if player
       return player.admin
     end
   end
-  return false
+  false
 end
 
 def hashOf(password)
-  return Digest::SHA512.hexdigest(settings.sha_salt + password)
+  Digest::SHA512.hexdigest(settings.sha_salt + password)
 end
 
 def setLoggedInSession(player)
   if player
     session['player_id'] = player.id.to_s
   else
-    session['player_id'] = ""
+    session['player_id'] = ''
   end
 end
 
 before do
-  pass if request.path_info == "/login/"
-  @logged_in_player = getCurrentPlayer()
-  @now = nowString()
-  @isAdmin = checkIsAdmin()
-  if session['player_id'] and session['player_id'] != ""
+  pass if request.path_info == '/login/'
+  @logged_in_player = getCurrentPlayer
+  @now = nowString
+  @isAdmin = checkIsAdmin
+  if session['player_id'] && session['player_id'] != ''
     @isAuthenticated = true
   end
-  if not @logged_in_player
-    redirect "/login/"
+  unless @logged_in_player
+    redirect '/login/'
   end
 end
 
 get '/logout/' do
   setLoggedInSession(nil)
-  redirect "/"
+  redirect '/'
 end
 
 get '/login/' do
-  haml :login, :layout => false
+  haml :login, layout: false
 end
 
 post '/login/' do
   name = params[:name]
   password = params[:password]
-  player = Player.first(:name => name)
+  player = Player.first(name: name)
   fail = true # Default to fail in case I make a mistake and allow a code path that doesn't catch a failure
   password_hash = hashOf(password)
   if player
@@ -315,17 +315,17 @@ post '/login/' do
     if Player.count == 0
       # We'll create an admin with the first char that logs in
       fail = false
-      player = Player.create(:name => name, :admin => true, :password_hash_sha512 => password_hash)
+      player = Player.create(name: name, admin: true, password_hash_sha512: password_hash)
       setLoggedInSession(player)
     else
       fail = true
     end
   end
   if fail
-    @message = "Login failed"
-    return haml :login, :layout => false
+    @message = 'Login failed'
+    return haml :login, layout: false
   end
-  redirect "/"
+  redirect '/'
 end
 
 get '/my/:year/:month/' do
@@ -336,7 +336,7 @@ get '/my/:year/:month/' do
   @pending_events = []
   @all_events = getFilteredEvents(monthNumber, @year, lambda{|event|
     participated = false
-    event.characters.each{|character| participated = true if character.player == @logged_in_player}
+    event.characters.each { |character| participated = true if character.player == @logged_in_player }
     if participated
       if event.approval
         @approved_events << event
@@ -346,8 +346,8 @@ get '/my/:year/:month/' do
     end
     event if participated})
   @points = @logged_in_player.points_from(@approved_events)
-  @previous_link = getPreviousLink(monthNumber, @year, "/my/")
-  @next_link = getNextLink(monthNumber, @year, "/my/")
+  @previous_link = getPreviousLink(monthNumber, @year, '/my/')
+  @next_link = getNextLink(monthNumber, @year, '/my/')
   haml :my
 end
 
@@ -363,23 +363,23 @@ get '/month/:year/:month/' do
 end
 
 get '/add_event/' do
-  @create_or_edit = "Create"
-  @submit_relative_url = "/add_event/"
-  @charactersArray = allCharactersJSON()
+  @create_or_edit = 'Create'
+  @submit_relative_url = '/add_event/'
+  @charactersArray = allCharactersJSON
   haml :edit_event
 end
 
-def allCharactersJSON()
-  '[' + Character.all(:active => true).map{|x| '"' + x.name + '"'}.join(", ") + ']'
+def allCharactersJSON
+  '[' + Character.all(active: true).map { |x| '"' + x.name + '"' }.join(', ') + ']'
 end
 
 def createOrEditEvent(params, create)
   description = params[:description]
   if params[:event_time] and not params[:event_time].empty?
     begin
-      event_time = Time.parse(params[:event_time] + "Z").utc
+      event_time = Time.parse(params[:event_time] + 'Z').utc
     rescue
-      @reason = "Invalid time format: " + params[:event_time] + $!.to_s
+      @reason = 'Invalid time format: ' + params[:event_time] + $ERROR_INFO.to_s
       return haml :error
     end
   else
@@ -390,36 +390,36 @@ def createOrEditEvent(params, create)
   event_type = params[:event_type]
   characters_string = params[:characters]
   if description.empty? or not $event_types.include?(event_type) or characters_string.empty? or corner_value <= 0.0 then
-    @reason = "Fill out all the fields"
+    @reason = 'Fill out all the fields'
     return haml :error
   end
   characters = []
   invalid_characters = []
   characters_string.split("\n").each {|x|
-    character = Character.first(:name => x.strip, :active => true)
+    character = Character.first(name: x.strip, active: true)
     if character then characters << character else invalid_characters << x end}
-  if not invalid_characters.empty? then
-    @reason = "Character doesn't exist (" + invalid_characters.join(", ") + ")"
+  unless invalid_characters.empty? then
+    @reason = "Character doesn't exist (" + invalid_characters.join(', ') + ')'
     return haml :error
   end
-  @charactersArray = allCharactersJSON()
+  @charactersArray = allCharactersJSON
   if create
-    submission = Submission.create(:player => @logged_in_player, :time => Time.new.utc)
-    @event = Event.create(:description => description, :event_time => event_time, :corner_value => corner_value, :event_type => event_type, :characters => characters, :submission => submission)
-    @message = "Event created"
-    @create_or_edit = "Edit"
-    @submit_relative_url = "/edit_event/" + @event.id.to_s + "/"
+    submission = Submission.create(player: @logged_in_player, time: Time.new.utc)
+    @event = Event.create(description: description, event_time: event_time, corner_value: corner_value, event_type: event_type, characters: characters, submission: submission)
+    @message = 'Event created'
+    @create_or_edit = 'Edit'
+    @submit_relative_url = '/edit_event/' + @event.id.to_s + '/'
     haml :edit_event
   else
-    @event = Event.first(:id => params[:id])
-    if not @event
-      @reason = "Event " + params[:id].to_s + "doesn't exist"
+    @event = Event.first(id: params[:id])
+    unless @event
+      @reason = 'Event ' + params[:id].to_s + "doesn't exist"
       haml :error
     end
-    @event.update(:description => description, :event_time => event_time, :corner_value => corner_value, :event_type => event_type, :characters => characters)
-    @message = "Event modified"
-    @create_or_edit = "Edit"
-    @submit_relative_url = "/edit_event/" + @event.id.to_s + "/"
+    @event.update(description: description, event_time: event_time, corner_value: corner_value, event_type: event_type, characters: characters)
+    @message = 'Event modified'
+    @create_or_edit = 'Edit'
+    @submit_relative_url = '/edit_event/' + @event.id.to_s + '/'
     haml :edit_event
   end
 end
@@ -429,13 +429,13 @@ post '/add_event/' do
 end
 
 get '/delete_event/:id/' do
-  event = Event.first(:id => params[:id])
-  if not checkIsAdmin()
-    @reason = "No access"
+  event = Event.first(id: params[:id])
+  unless checkIsAdmin
+    @reason = 'No access'
     return haml :error
   end
-  if not event
-    @reason = "Event " + id + " doesn't exist"
+  unless event
+    @reason = 'Event ' + id + " doesn't exist"
     return haml :error
   end
   event.attendances.destroy
@@ -444,99 +444,98 @@ get '/delete_event/:id/' do
 end
 
 get '/edit_event/:id/' do
-  @event = Event.first(:id => params[:id])
-  if not @event
-    @reason = "Event " + id + " doesn't exist"
+  @event = Event.first(id: params[:id])
+  unless @event
+    @reason = 'Event ' + id + " doesn't exist"
     return haml :error
   end
-  if checkIsAdmin() or (@event.submission.player == @logged_in_player and not @event.approval)
-    @create_or_edit = "Edit"
-    @submit_relative_url = "/edit_event/" + @event.id.to_s + "/"
-    @charactersArray = allCharactersJSON()
+  if checkIsAdmin || (@event.submission.player == @logged_in_player && !@event.approval)
+    @create_or_edit = 'Edit'
+    @submit_relative_url = '/edit_event/' + @event.id.to_s + '/'
+    @charactersArray = allCharactersJSON
     haml :edit_event
   else
-    @reason = "No access"
+    @reason = 'No access'
     return haml :error
   end
 end
 
 post '/edit_event/:id/' do
-  event = Event.first(:id => params[:id])
-  if checkIsAdmin() or event.allow_edit(@logged_in_player)
+  event = Event.first(id: params[:id])
+  if checkIsAdmin or event.allow_edit(@logged_in_player)
     return createOrEditEvent(params, false)
   else
-    @reason = "No access"
+    @reason = 'No access'
     return haml :error
   end
 end
 
 post '/add_player/' do
   @name = params[:name]
-  existing = Player.first(:name => @name)
-  if existing or not checkIsAdmin()
-    @reason = "You tried to add a player that already exists (name=" + @name + ")"
+  existing = Player.first(name: @name)
+  if existing or !checkIsAdmin
+    @reason = 'You tried to add a player that already exists (name=' + @name + ')'
     haml :error
   else
-    player = Player.create(:name => @name)
-    redirect "/edit_player/" + player.id.to_s + "/"
+    player = Player.create(name: @name)
+    redirect '/edit_player/' + player.id.to_s + '/'
     haml :add_player_success
   end
 end
 
 post '/add_character/' do
   @name = params[:name]
-  existing = Character.first(:name => @name, :active => true)
-  @player = Player.first(:id => params[:player_id])
-  if existing or not checkIsAdmin()
-    @reason = "You tried to add a character that already exists (name=" + @name + ")"
+  existing = Character.first(name: @name, active: true)
+  @player = Player.first(id: params[:player_id])
+  if existing or !checkIsAdmin
+    @reason = 'You tried to add a character that already exists (name=' + @name + ')'
     haml :error
-  elsif not @player
-    @reason = "Can't find the player you're trying to add a character to (id=" + params[:player_id] + ")"
+  elsif !@player
+    @reason = "Can't find the player you're trying to add a character to (id=" + params[:player_id] + ')'
     haml :error
   else
-    Character.create(:name => @name, :player => @player, :active => true)
+    Character.create(name: @name, player: @player, active: true)
     @message = "Character '" + @name + "' added to " + @player.name
     haml :edit_player
   end
 end
 
-def renderAdminPage()
-  @events = getPendingEvents()
-  @players = Player.all(:order => [:name], )
+def renderAdminPage
+  @events = getPendingEvents
+  @players = Player.all(order: [:name],)
   haml :admin
 end
 
 get '/approve_event/:id/' do
-  if not checkIsAdmin()
-    @reason = "Not Admin"
+  unless checkIsAdmin
+    @reason = 'Not Admin'
     return haml :error
   end
   @allow_edit = true
   @allow_approval = true
 
-  event = Event.first(:id => params[:id])
-  Approval.create(:player => @logged_in_player, :event => event)
-  renderAdminPage()
+  event = Event.first(id: params[:id])
+  Approval.create(player: @logged_in_player, event: event)
+  renderAdminPage
 end
-
 
 get '/admin/' do
   @allow_approval = true
   @allow_edit = true
   @allow_delete = true
-  if not checkIsAdmin()
-    @reason = "Not Admin"
+  unless checkIsAdmin
+    @reason = 'Not Admin'
     return haml :error
   end
-  renderAdminPage()
+  renderAdminPage
 end
 
 get '/delete_character/:id/' do
   id = params[:id]
-  character = Character.first(:id => id)
-  if character and checkIsAdmin()
-    character.update(:active => false)
-    @message = "Character " + character.name + " removed."
+  character = Character.first(id: id)
+  if character and checkIsAdmin
+    character.update(active: false)
+    @message = 'Character ' + character.name + ' removed.'
     @player = character.player
     haml :edit_player
   else
@@ -546,45 +545,45 @@ get '/delete_character/:id/' do
 end
 
 post '/admin_reset_password/' do
-  if not checkIsAdmin()
-    @reason = "Not an admin"
+  unless checkIsAdmin
+    @reason = 'Not an admin'
     haml :error
   end
-  @player = Player.first(:id => params[:player_id])
+  @player = Player.first(id: params[:player_id])
   if @player
     password_hash = hashOf(params[:password])
-    @player.update(:password_hash_sha512 => password_hash)
-    @message = "Updated password"
-    redirect "/admin/"
+    @player.update(password_hash_sha512: password_hash)
+    @message = 'Updated password'
+    redirect '/admin/'
   end
-  @reason = "No such player"
+  @reason = 'No such player'
   haml :error
 end
 
 post '/edit_player/:id/' do
   id = params[:id]
-  @player = Player.first(:id => id)
-  if @player and checkIsAdmin()
+  @player = Player.first(id: id)
+  if @player && checkIsAdmin
     admin = false
     if params[:admin]
       admin = true
     end
-    @player.update(:name => params[:name], :admin => admin)
-    @message = "Player updated"
+    @player.update(name: params[:name], admin: admin)
+    @message = 'Player updated'
     haml :edit_player
   else
-    @reason = "You tried to edit a player that doesn't exist (id=" + id + ")"
+    @reason = "You tried to edit a player that doesn't exist (id=" + id + ')'
     haml :error
   end
 end
 
 get '/edit_player/:id/' do
   id = params[:id]
-  @player = Player.first(:id => id)
-  if @player and checkIsAdmin()
+  @player = Player.first(id: id)
+  if @player && checkIsAdmin
     haml :edit_player
   else
-    @reason = "You tried to edit a player that doesn't exist (id=" + id + ")"
+    @reason = "You tried to edit a player that doesn't exist (id=" + id + ')'
   end
 end
 
